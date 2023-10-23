813e884ce9fe06fdc809a7c148b7656e49b0fe1f
Nacib Mariem <mariem.nacib@esprit.tn>
2023-10-23 17:26:28 +0100

    junit tests and sonarqube

--- src/main/java/tn/esprit/spring/khaddem/repositories/UniversiteRepository.java
+++ src/main/java/tn/esprit/spring/khaddem/repositories/UniversiteRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.spring.khaddem.entities.Universite;
 
+import java.util.Optional;
+
 @Repository
 public interface UniversiteRepository extends JpaRepository<Universite, Integer> {
+    Optional<Universite> findById(String nomUniv);
 }
