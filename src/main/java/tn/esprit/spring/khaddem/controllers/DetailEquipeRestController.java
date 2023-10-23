fd53451fa463b009f5496cb369a56c70def1dcf9
dina750 <dina.benmansour@esprit.tn>
2023-10-23 19:41:56 +0100

    sonarQube

--- src/main/java/tn/esprit/spring/khaddem/controllers/DetailEquipeRestController.java
+++ src/main/java/tn/esprit/spring/khaddem/controllers/DetailEquipeRestController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.khaddem.entities.DetailEquipe;
 import tn.esprit.spring.khaddem.services.IDetailEquipeService;
+import tn.esprit.spring.khaddem.dto.DetailEquipeDTO;
 
 import java.util.List;
 
@@ -28,14 +29,41 @@ public class DetailEquipeRestController {
 
     @PostMapping("/add-detail-equipe")
     @ResponseBody
-    public DetailEquipe addDetailEquipe(@RequestBody DetailEquipe detailEquipe) {
+    public DetailEquipe addDetailEquipe(@RequestBody DetailEquipeDTO detailEquipeDTO) {
+        // Créez une instance de DetailEquipe en copiant les données pertinentes
+        DetailEquipe detailEquipe = new DetailEquipe();
+        detailEquipe.setSalle(detailEquipeDTO.getSalle());
+        detailEquipe.setThematique(detailEquipeDTO.getThematique());
+
+        // Appelez la méthode d'ajout de votre service
         return detailEquipeService.addDetailEquipe(detailEquipe);
     }
 
+
     @PutMapping("/update-detail-equipe")
     @ResponseBody
-    public DetailEquipe updateDetailEquipe(@RequestBody DetailEquipe detailEquipe) {
-        return detailEquipeService.updateDetailEquipe(detailEquipe);
+    public DetailEquipeDTO updateDetailEquipe(@RequestBody DetailEquipeDTO detailEquipeDTO) {
+        // Créez une instance de DetailEquipe en copiant les données pertinentes
+        DetailEquipe detailEquipe = new DetailEquipe();
+        detailEquipe.setIdDetailEquipe(detailEquipeDTO.getIdDetailEquipe());
+        detailEquipe.setSalle(detailEquipeDTO.getSalle());
+        detailEquipe.setThematique(detailEquipeDTO.getThematique());
+
+        // Appelez la méthode de mise à jour de votre service
+        DetailEquipe updatedDetailEquipe = detailEquipeService.updateDetailEquipe(detailEquipe);
+
+        // Créez une nouvelle instance de DetailEquipeDTO pour renvoyer en réponse
+        DetailEquipeDTO updatedDTO = new DetailEquipeDTO();
+        updatedDTO.setIdDetailEquipe(updatedDetailEquipe.getIdDetailEquipe());
+        updatedDTO.setSalle(updatedDetailEquipe.getSalle());
+        updatedDTO.setThematique(updatedDetailEquipe.getThematique());
+
+        // Renvoyez le DTO en réponse à la requête
+        return updatedDTO;
     }
+
+
+
+
 
 }
