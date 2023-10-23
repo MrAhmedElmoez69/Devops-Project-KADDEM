19891daba3c5d01887a7a14dd2c8b07bb7a490c6
chedi landolsi <andoulsi.chedi@esprit.tn>
2023-10-23 20:18:35 +0100

    added test classes

---
+++ src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EquipeTest.java
@@ -0,0 +1,18 @@
+package tn.esprit.spring.khaddem.SpringbootwithunitTest;
+
+import org.junit.Test;
+import tn.esprit.spring.khaddem.entities.Equipe;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EquipeTest {
+
+    @Test
+    void testEquipeEntity(){
+        Equipe equipe = new Equipe();
+        equipe.setIdEquipe(1);
+        equipe.setNomEquipe("Test Equipe");
+
+        assertEquals(1, equipe.getIdEquipe());
+        assertEquals("Test Equipe", equipe.getNomEquipe());
+    }
+}
