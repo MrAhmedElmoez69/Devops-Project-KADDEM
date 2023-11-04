ecd1b93e640b604ef248e38a617dcb1c28c41fc9
MrAhmedElmoez69 <60655635+MrAhmedElmoez69@users.noreply.github.com>
2023-11-04 14:43:55 +0100

    fix

--- react-app/src/views/ExcelExport.js
+++
@@ -1,9 +0,0 @@
-import * as XLSX from 'xlsx';
-
-
-export function exportToExcel(products) {
-  const ws = XLSX.utils.json_to_sheet(products);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Products');
-  XLSX.writeFile(wb, 'products.xlsx');
-}
