02de304cef523690c06c1cbf975db1e2ee2870c9
Ahmed Elmoez Noomen <noomenahmed90@gmail.com>
2023-11-04 16:05:31 +0100

    fix

--- src/main/java/tn/esprit/spring/khaddem/KhaddemApplication.java
+++ src/main/java/tn/esprit/spring/khaddem/KhaddemApplication.java
@@ -2,10 +2,10 @@ package tn.esprit.spring.khaddem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableScheduling
+@ComponentScan(basePackages = "tn.esprit.spring.khaddem.services")
 public class KhaddemApplication {
 
     public static void main(String[] args) {
