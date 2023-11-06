0c1b4c0385a2db9437ece3fc88634c853c8db5b0
Ahmed Elmoez Noomen <noomenahmed90@gmail.com>
2023-11-06 13:08:47 +0100

    Fix & Coverage Code Dep

--- src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/DepartementTest.java
+++ src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/DepartementTest.java
@@ -1,15 +1,15 @@
 package tn.esprit.spring.khaddem.SpringbootwithunitTest;
 
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import tn.esprit.spring.khaddem.entities.Departement;
 
- class DepartementTest {
+class DepartementTest {
 
     @Test
-     void testDepartementEntity() {
+    void testDepartementEntity() {
         Departement departement = new Departement();
         departement.setIdDepartement(1);
         departement.setNomDepart("Test Department");
@@ -17,4 +17,43 @@ import tn.esprit.spring.khaddem.entities.Departement;
         assertEquals(1, departement.getIdDepartement());
         assertEquals("Test Department", departement.getNomDepart());
     }
+
+    @Test
+    void testDepartementEquality() {
+        Departement departement1 = new Departement();
+        departement1.setIdDepartement(1);
+        departement1.setNomDepart("Department A");
+
+        Departement departement2 = new Departement();
+        departement2.setIdDepartement(1);
+        departement2.setNomDepart("Department A");
+
+        assertEquals(departement1, departement2);
+    }
+
+
+    @Test
+    void testDepartementInequality() {
+        Departement departement1 = new Departement();
+        departement1.setIdDepartement(1);
+        departement1.setNomDepart("Department A");
+
+        Departement departement2 = new Departement();
+        departement2.setIdDepartement(2);
+        departement2.setNomDepart("Department B");
+
+        assertNotEquals(departement1, departement2);
+    }
+
+    @Test
+    void testSettersAndGetters() {
+        Departement departement = new Departement();
+        departement.setIdDepartement(1);
+        departement.setNomDepart("Test Department");
+
+        assertEquals(1, departement.getIdDepartement());
+        assertEquals("Test Department", departement.getNomDepart());
+
+    }
+
 }
