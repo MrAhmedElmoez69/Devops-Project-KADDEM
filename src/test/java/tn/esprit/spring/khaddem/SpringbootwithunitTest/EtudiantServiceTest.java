9be4968b9d4e6a46078a070e0bada595bd8ab922
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-11-12 11:49:52 +0100

    added test 2

--- src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EtudiantServiceTest.java
+++ src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EtudiantServiceTest.java
@@ -2,21 +2,27 @@ package tn.esprit.spring.khaddem.SpringbootwithunitTest;
 
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.khaddem.dto.EtudiantDTO;
-import tn.esprit.spring.khaddem.entities.Etudiant;
-import tn.esprit.spring.khaddem.entities.Niveau;
-import tn.esprit.spring.khaddem.entities.Specialite;
+import tn.esprit.spring.khaddem.entities.*;
+import tn.esprit.spring.khaddem.repositories.ContratRepository;
+import tn.esprit.spring.khaddem.repositories.EquipeRepository;
 import tn.esprit.spring.khaddem.repositories.EtudiantRepository;
 import tn.esprit.spring.khaddem.services.EtudiantServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
 
 @ExtendWith(MockitoExtension.class)
 public class EtudiantServiceTest {
@@ -25,6 +31,8 @@ public class EtudiantServiceTest {
 
     @Mock
     private EtudiantRepository etudiantRepository;
+    private ContratRepository contratRepository;
+    private EquipeRepository equipeRepository;
 
     @Test
     public void testRetrieveAllEtudiants() {
@@ -113,5 +121,63 @@ public class EtudiantServiceTest {
         assertEquals(expectedEtudiants, actualEtudiants);
     }
 
-    // Additional tests can be added for other methods as needed...
+    @Test
+    public void testUpdateEtudiant() {
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("Updated Etudiant");
+        when(etudiantRepository.existsById(etudiant.getIdEtudiant())).thenReturn(true);
+        when(etudiantRepository.save(etudiant)).thenReturn(etudiant);
+
+        Etudiant updatedEtudiant = etudiantService.updateEtudiant(etudiant);
+
+        assertEquals(etudiant, updatedEtudiant);
+    }
+
+    @Test
+    public void testRemoveEtudiant() {
+        int etudiantId = 1;
+        when(etudiantRepository.existsById(etudiantId)).thenReturn(true);
+        doNothing().when(etudiantRepository).deleteById(etudiantId);
+
+        etudiantService.removeEtudiant(etudiantId);
+
+        verify(etudiantRepository, times(1)).deleteById(etudiantId);
+    }
+
+    @Test
+    public void testAssignEtudiantToDepartement() {
+        int etudiantId = 1;
+        int departementId = 2;
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(etudiantId);
+        when(etudiantRepository.findById(etudiantId)).thenReturn(Optional.of(etudiant));
+        when(etudiantRepository.save(etudiant)).thenReturn(etudiant);
+
+        etudiantService.assignEtudiantToDepartement(etudiantId, departementId);
+
+        assertEquals(departementId, etudiant.getDepartement().getIdDepartement());
+    }
+
+    @Test
+    public void testAddAndAssignEtudiantToEquipeAndContract() {
+        int contratId = 1;
+        int equipeId = 2;
+        Etudiant etudiant = new Etudiant();
+        // Set up your etudiant, contrat, and equipe data
+        when(contratRepository.findById(contratId)).thenReturn(Optional.of(new Contrat()));
+        when(equipeRepository.findById(equipeId)).thenReturn(Optional.of(new Equipe()));
+        when(etudiantRepository.save(etudiant)).thenReturn(etudiant);
+
+        Etudiant resultEtudiant = etudiantService.addAndAssignEtudiantToEquipeAndContract(etudiant, contratId, equipeId);
+
+        assertNotNull(resultEtudiant);
+        // Add more assertions based on your implementation
+    }
+
+
+
+
+
+
 }
