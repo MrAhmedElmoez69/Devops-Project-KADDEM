a8df2e6ff5fa288980b268513b5fa9e7ea1af721
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-11-12 13:51:44 +0100

    test 12

--- src/main/java/tn/esprit/spring/khaddem/services/IEtudiantService.java
+++ src/main/java/tn/esprit/spring/khaddem/services/IEtudiantService.java
@@ -1,8 +1,6 @@
 package tn.esprit.spring.khaddem.services;
 
 import tn.esprit.spring.khaddem.entities.Etudiant;
-import tn.esprit.spring.khaddem.entities.Niveau;
-import tn.esprit.spring.khaddem.entities.Specialite;
 
 import java.util.List;
 
