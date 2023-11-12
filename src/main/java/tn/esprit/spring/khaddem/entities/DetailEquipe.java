e622c72fd076feafad0611df50da5b8458be8743
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-11-12 22:14:32 +0100

    test yasser

--- src/main/java/tn/esprit/spring/khaddem/entities/DetailEquipe.java
+++ src/main/java/tn/esprit/spring/khaddem/entities/DetailEquipe.java
@@ -28,10 +28,28 @@ public class DetailEquipe implements Serializable {
     @JsonIgnore
     private Equipe equipe;
 
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        DetailEquipe other = (DetailEquipe) obj;
+        return idDetailEquipe == other.idDetailEquipe
+                && salle == other.salle
+                && Objects.equals(thematique, other.thematique)
+                && Objects.equals(equipe, other.equipe);
+    }
+
+
     @Override
     public int hashCode() {
-        return Objects.hash(idDetailEquipe, salle, thematique);
+        return Objects.hash(idDetailEquipe, salle, thematique, equipe);
     }
+
 
 
 
