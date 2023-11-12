d97da24a43f932910273198d46a9a934176bd45e
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-11-12 11:33:52 +0100

    added test

--- src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EtudiantServiceTest.java
+++ src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EtudiantServiceTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.spring.khaddem.dto.EtudiantDTO;
 import tn.esprit.spring.khaddem.entities.Etudiant;
+import tn.esprit.spring.khaddem.entities.Niveau;
+import tn.esprit.spring.khaddem.entities.Specialite;
 import tn.esprit.spring.khaddem.repositories.EtudiantRepository;
 import tn.esprit.spring.khaddem.services.EtudiantServiceImpl;
 
@@ -54,4 +57,61 @@ public class EtudiantServiceTest {
 
         assertEquals(etudiant, addedEtudiant);
     }
+
+    @Test
+    public void testFindByDepartementIdDepartement() {
+        int departementId = 1;
+        List<Etudiant> expectedEtudiants = new ArrayList<>();
+        when(etudiantRepository.findByDepartementIdDepartement(departementId)).thenReturn(expectedEtudiants);
+
+        List<Etudiant> actualEtudiants = etudiantService.findByDepartementIdDepartement(departementId);
+
+        assertEquals(expectedEtudiants, actualEtudiants);
+    }
+
+    @Test
+    public void testFindByEquipesNiveau() {
+        Niveau niveau = Niveau.JUNIOR; // Replace with your actual enum value
+        List<Etudiant> expectedEtudiants = new ArrayList<>();
+        when(etudiantRepository.findByEquipesNiveau(niveau)).thenReturn(expectedEtudiants);
+
+        List<Etudiant> actualEtudiants = etudiantService.findByEquipesNiveau(niveau);
+
+        assertEquals(expectedEtudiants, actualEtudiants);
+    }
+
+    @Test
+    public void testRetrieveEtudiantsByContratSpecialite() {
+        Specialite specialite = Specialite.SECURITE; // Replace with your actual enum value
+        List<Etudiant> expectedEtudiants = new ArrayList<>();
+        when(etudiantRepository.retrieveEtudiantsByContratSpecialite(specialite)).thenReturn(expectedEtudiants);
+
+        List<Etudiant> actualEtudiants = etudiantService.retrieveEtudiantsByContratSpecialite(specialite);
+
+        assertEquals(expectedEtudiants, actualEtudiants);
+    }
+
+    @Test
+    public void testRetrieveEtudiantsByContratSpecialiteSQL() {
+        String specialite = "SECURITE"; // Replace with your actual value
+        List<Etudiant> expectedEtudiants = new ArrayList<>();
+        when(etudiantRepository.retrieveEtudiantsByContratSpecialiteSQL(specialite)).thenReturn(expectedEtudiants);
+
+        List<Etudiant> actualEtudiants = etudiantService.retrieveEtudiantsByContratSpecialiteSQL(specialite);
+
+        assertEquals(expectedEtudiants, actualEtudiants);
+    }
+
+    @Test
+    public void testGetEtudiantsByDepartement() {
+        int departementId = 1;
+        List<Etudiant> expectedEtudiants = new ArrayList<>();
+        when(etudiantRepository.findByDepartementIdDepartement(departementId)).thenReturn(expectedEtudiants);
+
+        List<Etudiant> actualEtudiants = etudiantService.getEtudiantsByDepartement(departementId);
+
+        assertEquals(expectedEtudiants, actualEtudiants);
+    }
+
+    // Additional tests can be added for other methods as needed...
 }
--- src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EtudiantTest.java
+++ src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/EtudiantTest.java
@@ -2,17 +2,69 @@ package tn.esprit.spring.khaddem.SpringbootwithunitTest;
 
 import org.junit.jupiter.api.Test;
 import tn.esprit.spring.khaddem.entities.Etudiant;
+import tn.esprit.spring.khaddem.entities.Option;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EtudiantTest {
     @Test
     void testEtudiantEntity() {
+        // Arrange
         Etudiant etudiant = new Etudiant();
         etudiant.setIdEtudiant(1);
         etudiant.setNomE("Test Etudiant");
 
+        // Act & Assert
         assertEquals(1, etudiant.getIdEtudiant());
         assertEquals("Test Etudiant", etudiant.getNomE());
+    }
+
+    @Test
+    void testEtudiantEntityWithOption() {
+        // Arrange
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("Test Etudiant");
+        etudiant.setOp(Option.GAMIX);
+
+        // Act & Assert
+        assertEquals(1, etudiant.getIdEtudiant());
+        assertEquals("Test Etudiant", etudiant.getNomE());
+        assertEquals(Option.GAMIX, etudiant.getOp());
+    }
+
+    @Test
+    void testEtudiantEntityToString() {
+        // Arrange
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("Test Etudiant");
+        etudiant.setOp(Option.GAMIX);
+
+        // Act & Assert
+        String expectedToString = "Etudiant{" +
+                "idEtudiant=" + etudiant.getIdEtudiant() +
+                ", prenomE='" + etudiant.getPrenomE() + '\'' +
+                ", nomE='" + etudiant.getNomE() + '\'' +
+                ", op=" + etudiant.getOp() +
+                ", departement=" + etudiant.getDepartement() +
+                ", equipes=" + etudiant.getEquipes() +
+                ", contrats=" + etudiant.getContrats() +
+                '}';
+        assertEquals(expectedToString, etudiant.toString());
+    }
+
+    @Test
+    void testEtudiantEntityWithDifferentOption() {
+        // Arrange
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("Test Etudiant");
+        etudiant.setOp(Option.SAE);
+
+        // Act & Assert
+        assertEquals(1, etudiant.getIdEtudiant());
+        assertEquals("Test Etudiant", etudiant.getNomE());
+        assertEquals(Option.SAE, etudiant.getOp());
     }
 }
