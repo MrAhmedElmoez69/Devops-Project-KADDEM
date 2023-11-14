ec4cd9a92a42fa6d4f9200b8e0208ac43e490dbc
chedi landolsi <andoulsi.chedi@esprit.tn>
2023-11-14 10:45:27 +0100

    added unit test again

--- src/main/java/tn/esprit/spring/khaddem/dto/EquipeDTO.java
+++ src/main/java/tn/esprit/spring/khaddem/dto/EquipeDTO.java
@@ -2,14 +2,15 @@ package tn.esprit.spring.khaddem.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import tn.esprit.spring.khaddem.entities.Niveau;
 
-
-
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 @Getter
 @Setter
 public class EquipeDTO {
-    private String nomEquipe;
     private Integer idEquipe;
-
-
+    private String nomEquipe;
+    @Enumerated(EnumType.STRING)
+    private Niveau niveau;
 }
