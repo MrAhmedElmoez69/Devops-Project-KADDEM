f58b4e4252f3ed6de030f7800675408605039a28
Safa_Mejri <mejri.safa@esprit.tn>
2023-11-16 18:15:19 +0100

    testService

--- src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/ContratServiceTest.java
+++ src/test/java/tn/esprit/spring/khaddem/SpringbootwithunitTest/ContratServiceTest.java
@@ -147,14 +147,13 @@ class ContratServiceTest {
 
         // Act
         Contrat result = contratService.addAndAffectContratToEtudiant(ce, nomE, prenomE);
-
         // Assert
         assertEquals(6, etudiant.getContrats().size(), "Le contrat n'a pas été correctement ajouté à la liste des contrats de l'étudiant");
-
         // Vérifiez que la méthode save n'a pas été appelée lorsque le nombre de contrats actifs est supérieur à 5
         verify(contratRepository, times(0)).save(ce);
         // Ajoutez d'autres assertions au besoin
     }
+
 
 
 
