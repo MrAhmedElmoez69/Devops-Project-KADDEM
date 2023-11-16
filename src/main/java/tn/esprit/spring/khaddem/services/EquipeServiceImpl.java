df3d7622755865f912683ab47581fa4ec3153044
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-11-16 23:06:58 +0100

    equipe test

--- src/main/java/tn/esprit/spring/khaddem/services/EquipeServiceImpl.java
+++ src/main/java/tn/esprit/spring/khaddem/services/EquipeServiceImpl.java
@@ -3,12 +3,9 @@ package tn.esprit.spring.khaddem.services;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import tn.esprit.spring.khaddem.entities.Equipe;
 import tn.esprit.spring.khaddem.repositories.ContratRepository;
 import tn.esprit.spring.khaddem.repositories.EquipeRepository;
 
-import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -19,29 +16,6 @@ public class EquipeServiceImpl implements IEquipeService{
 
     ContratRepository contratRepository;
 
-    @Override
-    public List<Equipe> retrieveAllEquipes() {
-        return equipeRepository.findAll();
-    }
-
-    @Transactional
-    public Equipe addEquipe(Equipe e) {
-
-        equipeRepository.save(e);
-        return e;
-    }
-
-    @Override
-    public Equipe updateEquipe(Equipe eq) {
-        equipeRepository.save(eq);
-        return eq;
-    }
-
-    @Override
-    public Equipe retrieveEquipe(Integer idEquipe) {
-
-        return equipeRepository.findById(idEquipe).orElse(null); // Or throw an exception if needed
-    }
 
 
 
--- src/main/java/tn/esprit/spring/khaddem/services/IEquipeService.java
+++ src/main/java/tn/esprit/spring/khaddem/services/IEquipeService.java
@@ -1,18 +1,10 @@
 package tn.esprit.spring.khaddem.services;
 
-import tn.esprit.spring.khaddem.entities.Equipe;
 
-import java.util.List;
 
 public interface IEquipeService {
 
-    List<Equipe> retrieveAllEquipes();
 
-    Equipe  addEquipe(Equipe  e);
-
-    Equipe updateEquipe (Equipe  e);
-
-    Equipe retrieveEquipe (Integer idEquipe);
 
 
 
