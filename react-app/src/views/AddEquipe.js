2d5b1f0b2794498579cbe98ecd729dd08b542b41
chedilandolsi <andoulsi.chedi@esprit.tn>
2023-11-17 23:30:46 +0100

    fixed errors

--- react-app/src/views/AddEquipe.js
+++ react-app/src/views/AddEquipe.js
@@ -3,34 +3,22 @@ import { Button, Form, FormGroup, Label, Input, Col, Card, CardHeader, CardBody,
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+const Niveau = {
+  JUNIOR: 'JUNIOR',
+  SENIOR: 'SENIOR',
+  EXPERT: 'EXPERT',
+};
 class CreateEquipe extends Component {
   state = {
     nomEquipe: '',
-    niveau: '', // Utilisez cette valeur pour stocker la sélection du niveau
+    niveau: '',
     errors: {},
-    showNotification: false,
   };
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  validateForm = () => {
-    const errors = {};
-    const { nomEquipe, niveau } = this.state;
-
-    if (!nomEquipe) {
-      errors.nomEquipe = 'Nom d\'équipe est requis';
-    }
-
-    if (!niveau) {
-      errors.niveau = 'Niveau est requis';
-    }
-
-    this.setState({ errors });
-    return Object.keys(errors).length === 0;
-  }
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -39,18 +27,20 @@ class CreateEquipe extends Component {
       const newEquipe = {
         nomEquipe: this.state.nomEquipe,
         niveau: this.state.niveau,
-        etudiants: [], // Ajoutez les étudiants si nécessaire
       };
 
       axios.post('http://localhost:8090/Kaddem/equipe/add-equipe', newEquipe)
         .then(response => {
-          console.log('Equipe added successfully:', response.data);
-          toast.success('Equipe added successfully');
-          // Optionally, you can redirect to the university list page or perform other actions
+          console.log('Equipe created:', response.data);
+
+          // Display a success notification
+          toast.success('Equipe created successfully');
         })
         .catch(error => {
-          console.error('Error creating Equipe:', error);
-          toast.error('An error occurred while creating the Equipe');
+          console.error('Error creating equipe:', error);
+
+          // Display an error notification
+          toast.error('An error occurred while creating the equipe');
         });
     }
   }
@@ -59,8 +49,8 @@ class CreateEquipe extends Component {
     const { errors } = this.state;
 
     return (
-      <div className="content d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-        <Col md="8">
+      <div className="content">
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
           <Card>
             <CardHeader>
               <CardTitle tag="h4">Create Equipe</CardTitle>
@@ -68,7 +58,7 @@ class CreateEquipe extends Component {
             <CardBody>
               <Form onSubmit={this.handleSubmit}>
                 <FormGroup>
-                  <Label for="nomEquipe">Nom d'équipe</Label>
+                  <Label for="nomEquipe">Equipe Name</Label>
                   <Input
                     type="text"
                     name="nomEquipe"
@@ -76,11 +66,18 @@ class CreateEquipe extends Component {
                     value={this.state.nomEquipe}
                     onChange={this.handleChange}
                     className={errors.nomEquipe ? 'is-invalid' : ''}
+                    style={{
+                      padding: "10px",
+                      border: "2px solid #ccc",
+                      borderRadius: "20px",
+                      fontSize: "16px",
+                      outline: "none",
+                    }}
                   />
                   {errors.nomEquipe && <div className="invalid-feedback">{errors.nomEquipe}</div>}
                 </FormGroup>
                 <FormGroup>
-                  <Label for="niveau">Niveau</Label>
+                  <Label for="niveau">Equipe Niveau</Label>
                   <Input
                     type="select"
                     name="niveau"
@@ -88,22 +85,31 @@ class CreateEquipe extends Component {
                     value={this.state.niveau}
                     onChange={this.handleChange}
                     className={errors.niveau ? 'is-invalid' : ''}
+                    style={{
+                      padding: "10px",
+                      border: "2px solid #ccc",
+                      borderRadius: "20px",
+                      fontSize: "16px",
+                      outline: "none",
+                    }}
                   >
-                    <option value="">Sélectionnez un niveau</option>
-                    <option value="JUNIOR">JUNIOR</option>
-                    <option value="SENIOR">SENIOR</option>
-                    <option value="EXPERT">EXPERT</option>
+                    <option value="">Select a niveau</option>
+                    {Object.values(Niveau).map((value) => (
+                      <option key={value} value={value}>
+                        {value}
+                      </option>
+                    ))}
                   </Input>
                   {errors.niveau && <div className="invalid-feedback">{errors.niveau}</div>}
                 </FormGroup>
-                <Button color="primary" type="submit">Create Equipe</Button>
+                <FormGroup className="text-center">
+                  <Button color="primary" type="submit" style={{ borderRadius: "20px" }}>Create</Button>
+                </FormGroup>
               </Form>
             </CardBody>
-            <CardFooter>
-            </CardFooter>
+            <CardFooter></CardFooter>
           </Card>
         </Col>
-
         <ToastContainer />
       </div>
     );
--- react-app/src/views/ListEquipe.js
+++ react-app/src/views/ListEquipe.js
@@ -1,96 +1,53 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Table, Button, Input } from "reactstrap";
+import { Table} from "reactstrap";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Link } from "react-router-dom";
+
 
 class EquipeList extends Component {
   state = {
     equipes: [],
-    searchTerm: "",
   };
 
   componentDidMount() {
+    this.loadEquipes();
+  }
+
+  loadEquipes = () => {
     axios
-      .get("http://localhost:8090/equipe/retrieve-all-equipes")
+      .get("http://localhost:8090/Kaddem/equipe/retrieve-all-equipes")
       .then((response) => {
         const equipes = response.data;
         this.setState({ equipes });
-        toast.success("Welcome to Equipe list");
+        toast.success("Welcome to the equipe list");
       })
       .catch((error) => {
-        console.error("Error fetching Equipes:", error);
+        console.error("Error fetching equipes:", error);
       });
-  }
-
-  handleDelete = (equipeId) => {
-    axios
-      .delete(`http://localhost:8090/equipe/${equipeId}`)
-      .then((response) => {
-        this.setState((prevState) => ({
-          equipes: prevState.equipes.filter(
-            (equipe) => equipe.idEquipe !== equipeId
-          ),
-        }));
-        toast.success("Equipe deleted successfully");
-        console.log("Equipe deleted:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error deleting Equipe:", error);
-      });
-  };
-
-  handleSearchChange = (e) => {
-    this.setState({ searchTerm: e.target.value });
   };
 
   render() {
-    const { equipes, searchTerm } = this.state;
-    const filteredEquipes = equipes.filter((equipe) =>
-      equipe.nomEquipe.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    const { equipes } = this.state;
+
     return (
       <div className="content">
         <ToastContainer />
         <h1 className="mb-4">Equipe List</h1>
-        <Input
-          type="text"
-          placeholder="Search for an equipe"
-          value={searchTerm}
-          onChange={this.handleSearchChange}
-          style={{ width: "100%", marginBottom: "10px" }}
-        />
         <Table striped bordered hover responsive>
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Nom d'équipe</th>
+              <th>Equipe Name</th>
               <th>Niveau</th>
-              <th style={{ width: "250px" }}>Actions</th>
+              {/* Add more columns for other equipe information */}
             </tr>
           </thead>
           <tbody>
-            {filteredEquipes.map((equipe) => (
+            {equipes.map((equipe) => (
               <tr key={equipe.idEquipe}>
-                <td>{equipe.idEquipe}</td>
                 <td>{equipe.nomEquipe}</td>
                 <td>{equipe.niveau}</td>
-                <td>
-                  <Button
-                    color="danger"
-                    onClick={() => this.handleDelete(equipe.idEquipe)}
-                  >
-                    Delete
-                  </Button>
-                  {/* Ajoutez le lien vers la page d'édition si nécessaire */}
-                  <Link
-                    to={`/edit-equipe/${equipe.idEquipe}`}
-                    className="btn btn-primary ml-2"
-                  >
-                    Edit
-                  </Link>
-                </td>
+                {/* Add more cells for other equipe information */}
               </tr>
             ))}
           </tbody>
